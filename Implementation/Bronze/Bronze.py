f28a0cc66ee50637b7cc991df01b495931ce0af1
EoJin-Kim <62640679+EoJin-Kim@users.noreply.github.com>
2021-04-13 22:32:11 +0900

    COMMIT

--- Implementation/Bronze/Bronze.py
+++ Implementation/Bronze/Bronze.py
@@ -1,2 +1,9 @@
-n=int(input(),8)
-print("{0:b}".format(n))
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+aSum=sum(a)
+bSum=sum(b)
+
+if aSum>=bSum:
+    print(aSum)
+else:
+    print(bSum)
